171ed036af98f95883f57ec79195e0442a1a3592
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-24 23:52:28 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,14 @@
 def hello_t(names)
   counter = 0
-
+if block_given?
  while counter <= names.length - 1
  yield(names[counter])
 counter += 1
 end
 names
+else
+puts  "Hey! No block was given!"
+end
 
 
 
