36a4fc2058f5b770f56faac10ea3ce8dab192238
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-24 23:55:02 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
+def hello_t(names)
+  counter = 0
+if block_given?
+ while counter <= names.length - 1
+ yield(names[counter])
+counter += 1
+end
+names
+else
+puts  "Hey! No block was given!"
+end
+
+
 
 end
 
 # call your method here!
-
